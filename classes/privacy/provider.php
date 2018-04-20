a88a59a287f11e12973727c3611ac8f4faf8a967
justinhunt <bitwalkerjapan@gmail.com>
2018-04-20 09:14:58 +0900

    added polyfill for null provider

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -34,6 +34,8 @@ defined('MOODLE_INTERNAL') || die();
  */
 class provider implements \core_privacy\local\metadata\null_provider {
 
+    use \core_privacy\local\legacy_polyfill;
+
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
